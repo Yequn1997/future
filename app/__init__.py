1b0efbf9a1b162a6834f48a502c0c0f6261d6e7f
Yequn1997 <1435074962@qq.com>
2022-02-25 18:27:09 +0800

    debugtoolerbar

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,14 @@ from flask_mail import Mail
 from settings import config
 from flask_login import LoginManager
 from flask_pagedown import PageDown
+from flask_debugtoolbar import DebugToolbarExtension
 
 bootstrap = Bootstrap()
 moment = Moment()
 mail = Mail()
 db = SQLAlchemy()
 pagedown = PageDown()
+toolbar = DebugToolbarExtension()
 
 
 login_manager = LoginManager()
@@ -29,6 +31,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     pagedown.init_app(app)
+    toolbar.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
